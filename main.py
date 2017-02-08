51754c28478257accd75d5c4c60c7037f395556e
user <user@UG-30-kawano-mac.local>
2017-02-08 16:13:19 +0900

    modify main.py

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ def get(event, context):
     
     html = '''
 <head>
+<style>
+h1 {
+    color: red;
+}
+</style>
 </head>
 <body>
-<h1>Test4</h1>
+<h1>Test</h1>
+<p>Version 5</p>
 </body>
 </html>
 '''[1:-1]
