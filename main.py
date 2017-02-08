da064af5e6c6346a7e7bd7fd7d0b11d0e727f684
user <user@UG-30-kawano-mac.local>
2017-02-08 16:22:03 +0900

    modify main.py

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ def get(event, context):
     html = '''
 <head>
 <style>
+.test-div {
+    background: rgba(255,255,255,0.5)
+}
+
 h1 {
     color: red;
 }
 </style>
 </head>
 <body>
-<h1>Test</h1>
-<p>Version 5</p>
+<div class="test-div">
+    <h1>Test</h1>
+    <p>Version 5</p>
+</div>
 </body>
 </html>
 '''[1:-1]
